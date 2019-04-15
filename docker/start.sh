4c722c2f47c3a3531c4093e1631171f9902306cc
Andrew Yourtchenko <ayourtch@gmail.com>
2019-04-15 12:13:53 +0000

    move to using testgerrit.myvpp.net

--- docker/start.sh
+++ docker/start.sh
@@ -2,9 +2,9 @@
 EXECSTART=`date`
 echo Arguments: $1 $2 $3 $4 $5
 echo EXECSTART: $EXECSTART
-git clone http://10.0.3.18:8080/vpp
+git clone http://testgerrit.myvpp.net/testvpp
 cd vpp
-git fetch http://10.0.3.18:8080/vpp $1 && git checkout FETCH_HEAD
+git fetch http://testgerrit.myvpp.net/testvpp $1 && git checkout FETCH_HEAD
 UNATTENDED=y make install-dep test
 EXECEND=`date`
 echo EXECEND: $EXECEND
