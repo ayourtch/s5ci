dd384b211902a6ec000e8374efc30803569fca25
Andrew Yourtchenko <ayourtch@gmail.com>
2019-08-28 10:54:15 +0000

    add config printing into check-config. Not quite a "check" but still.

--- go-s5ci/check-config.go
+++ go-s5ci/check-config.go
@@ -5,12 +5,25 @@ package main
 import (
 	"errors"
 	"fmt"
+	"gopkg.in/yaml.v2"
+	"log"
 )
 
 type CheckConfigCommand struct{}
 
 func (command *CheckConfigCommand) Execute(args []string) error {
+	c := &S5ciOptions.Config
+	fmt.Println("Checking config")
+	d, err := yaml.Marshal(c)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	fmt.Println(string(d))
+
 	var ErrShowHelpMessage = errors.New("check the configuration")
-	fmt.Println("Test")
-	return ErrShowHelpMessage
+	if false {
+		return ErrShowHelpMessage
+	} else {
+		return nil
+	}
 }
