d63df9d40435f65f51815a197bf03b809e3066c3
Andrew Yourtchenko <ayourtch@gmail.com>
2019-09-02 14:37:59 +0000

    go: implement set-status

--- go-s5ci/set-status.go
+++ go-s5ci/set-status.go
@@ -1,16 +1,25 @@
 package main
 
-// package commands
-
 import (
-	"errors"
-	"fmt"
+//"fmt"
 )
 
-type SetStatusCommand struct{}
+type SetStatusCommand struct {
+	JobID   string `short:"j" long:"job-id" description:"job ID (group_name/number)" required:"true"`
+	Message string `short:"m" long:"message" description:"status message" required:"true"`
+}
 
 func (command *SetStatusCommand) Execute(args []string) error {
-	var ErrShowHelpMessage = errors.New("list jobs command invoked")
-	fmt.Println("Test")
-	return ErrShowHelpMessage
+	job, err := DbGetJob(command.JobID)
+	if err != nil {
+		panic(err)
+	}
+	db := DbOpen()
+	defer DbClose(&db)
+	s5now := S5Now()
+	job.Status_Message = command.Message
+	job.Status_Updated_At = &s5now
+	DbSaveJob(&db, job)
+	RegenerateJobHtml(command.JobID)
+	return nil
 }
