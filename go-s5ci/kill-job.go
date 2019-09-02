4efae3c9f8bd38f92b93af2b31a2c2fbe4527f85
Andrew Yourtchenko <ayourtch@gmail.com>
2019-09-02 15:55:27 +0000

    go: implement kill-job

--- go-s5ci/kill-job.go
+++ go-s5ci/kill-job.go
@@ -3,14 +3,35 @@ package main
 // package commands
 
 import (
-	"errors"
 	"fmt"
+	"syscall"
 )
 
-type KillJobCommand struct{}
+type KillJobCommand struct {
+	JobID string `short:"j" long:"job-id" description:"job ID (group_name/number)" required:"true"`
+}
+
+func DoKillJob(job_id string, terminator string) {
+	job, err := DbGetJob(job_id)
+	if err != nil {
+		panic(err)
+	}
+	db := DbOpen()
+	defer DbClose(&db)
+	if job.Command_Pid != nil {
+		fmt.Printf("Requested to kill a job, sending signal to pid %d from job %s", job.Command_Pid, job_id)
+		syscall.Kill(*job.Command_Pid, syscall.SIGTERM)
+		if job.Finished_At == nil {
+			s5now := S5Now()
+			job.Status_Message = fmt.Sprintf("Terminated by the %s", terminator)
+			job.Status_Updated_At = &s5now
+			DbSaveJob(&db, job)
+			RegenerateJobHtml(job_id)
+		}
+	}
+}
 
 func (command *KillJobCommand) Execute(args []string) error {
-	var ErrShowHelpMessage = errors.New("Kill job")
-	fmt.Println("Test")
-	return ErrShowHelpMessage
+	DoKillJob(command.JobID, "S5CI CLI")
+	return nil
 }
