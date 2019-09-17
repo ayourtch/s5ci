888e17fdd37d89aee37699a9e866a0c79b27962b
Andrew Yourtchenko <ayourtch@gmail.com>
2019-09-17 13:00:41 +0000

    go: add an option to generate "interesting" job list for rsync back

--- go-s5ci/list-jobs.go
+++ go-s5ci/list-jobs.go
@@ -3,19 +3,57 @@ package main
 // package commands
 
 import (
-	"errors"
+	"bufio"
 	"fmt"
+	"os"
 )
 
 type ListJobsCommand struct {
+	TimeHorizonSec    int    `short:"t" long:"time-horizon" description:"Time horizon for stopped jobs, seconds"`
+	RsyncFileListName string `short:"r" long:"rsync-file-list" description:"File list for rsync"`
 }
 
 func (command *ListJobsCommand) Execute(args []string) error {
-	var ErrShowHelpMessage = errors.New("list jobs command invoked")
-	fmt.Println("Test")
-	jobs := DbGetActiveJobs()
-	for i, job := range jobs {
-		fmt.Printf("job %d: %s\n", i, job)
+	//	var ErrShowHelpMessage = errors.New("list jobs command invoked")
+	jobs := DbGetAllJobs()
+	w := bufio.NewWriter(os.Stdout)
+	rsync_output := false
+	if command.RsyncFileListName != "" {
+		rsync_output = true
+		f, err := os.Create(command.RsyncFileListName)
+		if err != nil {
+			panic(err)
+		}
+		w = bufio.NewWriter(f)
 	}
-	return ErrShowHelpMessage
+	job_group_seen := make(map[string]bool)
+	ts_now := UnixTimeNow()
+	if rsync_output {
+		fmt.Fprintf(w, "exclude workspace\n")
+	}
+	for i, job := range jobs {
+		if job.Finished_At != nil {
+			if job.Finished_At.UnixTimestamp() < ts_now-command.TimeHorizonSec {
+				continue
+			}
+		}
+		if rsync_output {
+			if !job_group_seen[job.Job_Group_Name] {
+				fmt.Fprintf(w, "include %s\n", job.Job_Group_Name)
+				fmt.Fprintf(w, "include %s/*\n", job.Job_Group_Name)
+				job_group_seen[job.Job_Group_Name] = true
+			}
+			fmt.Fprintf(w, "include %s/%d/**\n", job.Job_Group_Name, job.Instance_ID)
+		} else {
+			fmt.Printf("job %d: %s\n", i, job)
+		}
+	}
+	if rsync_output {
+		fmt.Fprintf(w, "include /index*.html\n")
+		fmt.Fprintf(w, "include /heartbeat.json\n")
+		fmt.Fprintf(w, "exclude *\n")
+	}
+	w.Flush()
+	return nil
+	//return ErrShowHelpMessage
 }
