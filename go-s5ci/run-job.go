0ec1fffd794ac5224df273741c0aee84396eea51
Andrew Yourtchenko <ayourtch@gmail.com>
2019-10-21 18:11:50 +0000

    fix the arguments to run-job

--- go-s5ci/run-job.go
+++ go-s5ci/run-job.go
@@ -6,8 +6,8 @@ import (
 
 type RunJobCommand struct {
 	Command        string `short:"c" long:"command" description:"command to run" required:"true"`
-	Changeset_ID   int    `short:"s" long:"changeset-id" description:"changeset id" required:"true"`
-	Patchset_ID    int    `short:"p" long:"patchset-id" description:"patchset id" required:"true"`
+	Changeset_ID   int    `short:"s" long:"changeset-id" env:"S5CI_GERRIT_CHANGESET_ID" description:"changeset id" required:"true"`
+	Patchset_ID    int    `short:"p" long:"patchset-id" env:"S5CI_GERRIT_PATCHSET_ID" description:"patchset id" required:"true"`
 	TriggerEventID string `short:"t" long:"trigger-event-id" description:"Trigger event ID" required:"false"`
 }
 
